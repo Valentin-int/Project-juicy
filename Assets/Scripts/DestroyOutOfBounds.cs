3a8b5a01caf38e73b240ad2ce06cd1870c918d88
Valentin LAY <valentin.lay98@gmail.com>
2022-05-18 21:25:32 +0200

    Destruction des ennemis et du joueur
    - Destruction des ennemis si ils sortent du terrain
    - Ajout d'un script permettant la perte de point de vie du joueur en fonction de l'ennemi avec lequel il reste en collision, quand ses points de vie sont à 0, il meurt

--- Assets/Scripts/DestroyOutOfBounds.cs
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -2,12 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyController : MonoBehaviour
+public class DestroyOutOfBounds : MonoBehaviour
 {
     // Variable of class
-    public float speed = 10;
-    public float life;
-    public float damage;
+    private float outOfBound = -17;
 
     // Start is called before the first frame update
     void Start()
@@ -18,6 +16,9 @@ public class EnemyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.back * Time.deltaTime * speed);
+        if (transform.position.z < outOfBound)
+        {
+            Destroy(gameObject);
+        }
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,13 +7,18 @@ public class PlayerController : MonoBehaviour
     // Variable of class
     public float speed = 10;
     public float horizontalInput;
+    public float life = 3;
+
+    public EnemyController enemyController;
+    public EnemyController enemyController1;
+    public EnemyController enemyController2;
 
     private float xRange = 7;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        enemyController = GameObject.Find("Enemy").GetComponent<EnemyController>();
     }
 
     // Update is called once per frame
@@ -23,5 +28,27 @@ public class PlayerController : MonoBehaviour
 
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
         transform.position = new Vector3(Mathf.Clamp(transform.position.x, -xRange, xRange), transform.position.y, transform.position.z);
+
+        if (life <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("hello");
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            life -= enemyController.damage;
+        }
+        if (other.gameObject.CompareTag("Enemy 1"))
+        {
+            life -= enemyController1.damage;
+        }
+        if (other.gameObject.CompareTag("Enemy 2"))
+        {
+            life -= enemyController2.damage;
+        }
     }
 }
