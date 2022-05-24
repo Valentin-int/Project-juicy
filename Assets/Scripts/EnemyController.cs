cb95cb8476a742351d7b756204cca1f6f6cb9c1b
Valentin LAY <valentin.lay98@gmail.com>
2022-05-24 19:18:12 +0200

    Création de 2 prefabs d'ennemis différents
    un que l'on doit esquiver en passant a l'intérieur
    et l'autre ou l'on doit sauter au dessus

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -6,8 +6,6 @@ public class EnemyController : MonoBehaviour
 {
     // Variable of class
     public float speed = 10;
-    public float life;
-    public float damage;
 
     // Start is called before the first frame update
     void Start()
